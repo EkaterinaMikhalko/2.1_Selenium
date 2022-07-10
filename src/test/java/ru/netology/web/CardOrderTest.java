6689cea6dd32236292530e1fb8c080ea8f571266
EkaterinaMikhalko <mikhalkoea@gmail.com>
2022-07-11 01:38:19 +0300

    Headless

--- src/test/java/ru/netology/web/CardOrderTest.java
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -22,6 +22,11 @@ class CardOrderTest {
     @BeforeEach
     void setup() {
         driver = WebDriverManager.chromedriver().create();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
